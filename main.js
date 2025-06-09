d9f4cf6cf0a3d0352193b89961d78018d4bf8e95
NikollasEdu <nikollas.padilha@escola.pr.gov.br>
2025-06-09 07:32:05 -0300

    .

--- main.js
+++ main.js
@@ -5,9 +5,18 @@ const jump = () => {
     mario.classlist.add("jump");
 
     setTimeout(() => {
-        mario.classList.remove("jump")
+        mario.classList.remove("jump");
     }
     ,500);
+}
+
+const loop = setInterval (() => {
+const pipePosition = pipe.offsetLeft;
+const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+
+    if(pipePosition) <= 120 && pipePosition > 0 && marioPosition < 80) {
+        
+    }
 }
 
 document.addEventListener("keydown", jump);
