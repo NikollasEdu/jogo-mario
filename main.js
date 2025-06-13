b9f2e1fc6171a9e6a250a7f38774670339546eb2
NikollasEdu <nikollas.padilha@escola.pr.gov.br>
2025-06-13 07:39:59 -0300

    .

--- main.js
+++ main.js
@@ -17,7 +17,16 @@ const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
     if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = "none";
         pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = "none";
+        mario.style.left = `${marioPosition}px`;
+
+        mario.src = "./img/game-over.png";
+        mario.style.width = "75px";
+        mario.style.marginLeft = "50px";
+
+        clearInterval(loop);
     }
-}
+},10);
 
 document.addEventListener("keydown", jump);
